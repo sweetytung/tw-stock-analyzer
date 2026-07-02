067935858c53e43495700b1216c6611f177843fd
sweetytung <sweetytung@gmail.com>
2026-07-02 19:46:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from services.stock_service import get_top20_volume
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
+@app.route("/top20")
+def top20():
+    date = request.args.get("date")
+    result = get_top20_volume(date)
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
