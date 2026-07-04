2d6dd93b34a88ff847411703652c7616a907531b
sweetytung <sweetytung@gmail.com>
2026-07-04 08:52:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from services.stock_service import get_top20_volume, get_top20_detail
+from services.stock_service import get_top20_volume, get_stock_detail
 
 app = Flask(__name__)
 
@@ -16,10 +16,11 @@ def top20():
     date = request.args.get("date")
     return jsonify(get_top20_volume(date))
 
-@app.route("/top20/detail")
-def top20_detail():
+@app.route("/stock/<stock_id>")
+def stock_detail(stock_id):
     date = request.args.get("date")
-    return jsonify(get_top20_detail(date))
+    name = request.args.get("name", "")
+    return jsonify(get_stock_detail(stock_id, name, date))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
